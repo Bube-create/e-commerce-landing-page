702f01d2fe0e2d3ea1e007992995f4fd50c93be3
Ebube <53101939+Bube-create@users.noreply.github.com>
2022-05-18 20:00:34 +0100

    Update Description.js

--- src/Description.js
+++ src/Description.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import React from "react";
-import CartControl from "./CartControl";
+import CartControl from "./CartControl.js";
 
 const Description = () => {
 	return (
